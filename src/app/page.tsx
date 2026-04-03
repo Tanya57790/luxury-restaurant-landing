bdf1960dce8a2db612dd314170702a21847922d4
Tatyana Makhortova <tmahortova67@gmail.com>
2026-04-04 03:21:17 +0500

    refactor(page): clean unnecessary comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-// import { Header } from '@/components/ui/Header';
-// import { Hero } from '@/components/ui/Hero';
-// import { About } from '@/components/ui/About';
-// import { OurCafe } from '@/components/ui/OurCafe';
-// import { Reservation } from '@/components/ui/Reservation';
-// import { CafeMenu } from '@/components/ui/CafeMenu';
-// import { Gallery } from '@/components/ui/Gallery';
-// import { Footer } from '@/components/ui/Footer';
 import { Sections } from '@/components/ui/Sections';
 
 export default function Home() {
